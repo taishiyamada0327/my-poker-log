469de744f40f9892af92c05422dc1c698f8988d7
taishi-yamada <taishi.37.0327@gmail.com>
2023-06-26 16:14:25 +0900

    add results show component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import ResultCreate from "./components/ResultCreate";
+import ResultList from "./components/ResultList";
+
+export type Results = {
+  id: number;
+  title: string
+}
 
 const App = () => {
-  const [result, setResult] = useState([]);
+  const [results, setResults] = useState<Results[]>([]);
 
   const createResult = (title: string) => {
-    console.log(`Need to add result with: ${title}`);
+    const updateResults = [
+      ...results,
+      {
+        id: Math.round(Math.random() * 9999),
+        title
+      }
+    ];
+    setResults(updateResults);
   };
 
   return(
     <div>
+      <ResultList results={results} />
       <ResultCreate onCreate={createResult} />
     </div>
   );
--- app/src/components/ResultShow.tsx
+++ app/src/components/ResultShow.tsx
@@ -1,5 +1,11 @@
-function ResultShow() {
-  return <div>ResultShow</div>
+import { Results } from "../App";
+
+type props = {
+  result: Results
+}
+
+const ResultShow: React.FC<props> = ({ result }) => {
+  return <div className="result-show">{result.title}</div>
 }
 
 export default ResultShow;
